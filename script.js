280ce22fb4adf167d01fd97d4ba9a67f08498b2f
Jorden <jordendickerson24@gmail.com>
2024-05-18 18:21:07 -0600

    added styles

--- script.js
+++ script.js
@@ -11,21 +11,17 @@ fetch(url)
     for (let i = 0; i < business.length; i++) {
       console.log(business[i].name);
       // You must include your javascript below to display the following information:
-      // 1. business names, 2.business image,
-      // 3. business locations, 4. business descriptions
       cardsContainer.innerHTML += `
       <section class="card">
+      <div class="left">
         <h2>${business[i].name}</h2>
-        <p>${business[i].description}</p>
-        <img src="${business[i].imageurl}" alt="business card image" />
+          <p>${business[i].description}</p>
+          </div>
+        <div class="right">
+          <img src="${business[i].imageurl}" alt="business card image" />
+        </div>
         <address>${business[i].address}</address>
       </section>
       `;
-      // Here is an example for the name to start you off.
-      // Creates card and places business name in h2 element
-      // Adds a classname to the section element above
-      // Create the image location, you can look up how to add image using javascript
-      // Include a business location
-      // Include a business Description
     }
   });
